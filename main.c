4ce5bf86409570c078317eba67afb88db0a27bf6
David Schwartzberg <dschwarzberg50@stuy.edu>
2024-11-25 14:48:20 -0500

    started to fork:

--- main.c
+++ main.c
@@ -3,6 +3,25 @@
 #include <unistd.h>
 
 int main() {
-  printf("yello\n");
+  printf("%d about to create 2 child processes\n", getpid());
+  pid_t p;
+  p = fork();
+  if(p<0){
+    perror("fork fail");//output to stderr instead of stdout
+    exit(1);
+  } else if ( p == 0){
+      printf("Hello from Child!\n");
+  }else{
+      printf("Hello from Parent!\n");
+      p = fork();
+      if(p<0){
+        perror("fork fail");//output to stderr instead of stdout
+        exit(1);
+      } else if ( p == 0){
+          printf("Hello from Child!\n");
+      }else{
+          printf("Hello from Parent!\n");
+      }
+  }
   return 0;
 }
