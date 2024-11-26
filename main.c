07713f08d56a9471beddc2250b319d835f166ec7
David Schwartzberg <dschwarzberg50@stuy.edu>
2024-11-26 14:17:22 -0500

    each child printing out random

--- main.c
+++ main.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 int main() {
   printf("%d about to create 2 child processes\n", getpid());
   pid_t p;
   p = fork();
-  if(p<0){
-    perror("fork fail");//output to stderr instead of stdout
+  if (p == -1) {
+    perror("Failed to fork");
     exit(1);
-  } else if ( p == 0){
-      printf("Hello from Child!\n");
-  }else{
-      printf("Hello from Parent!\n");
-      p = fork();
-      if(p<0){
-        perror("fork fail");//output to stderr instead of stdout
-        exit(1);
-      } else if ( p == 0){
-          printf("Hello from Child!\n");
-      }
+  } else if (p == 0) {
+    pid_t pid1 = getpid();
+    srand(pid1);
+    int time1 = (rand()%4)+1;
+    printf("%d: %d sec\n", pid1, time1);
+  } else {
+    printf("Hello from Parent!\n");
+    p = fork();
+    if (p == -1) {
+      perror("Failed to fork");
+      exit(1);
+    } else if (p == 0) {
+      pid_t pid2 = getpid();
+      srand(pid2);
+      int time2 = (rand()%4)+1;
+      printf("%d: %d sec\n", pid2, time2);
+    }
   }
+
+
+
   return 0;
 }
