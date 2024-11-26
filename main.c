51074f2b21fc619e65d487ae28f1fab7a3b12e1b
David Schwartzberg <dschwarzberg50@stuy.edu>
2024-11-26 14:20:55 -0500

    children print after finishing

--- main.c
+++ main.c
@@ -13,8 +13,10 @@ int main() {
   } else if (p == 0) {
     pid_t pid1 = getpid();
     srand(pid1);
-    int time1 = (rand()%4)+1;
+    unsigned int time1 = (rand()%4)+1;
     printf("%d: %d sec\n", pid1, time1);
+    sleep(time1);
+    printf("%d finished after %d sec\n", pid1, time1);
   } else {
     printf("Hello from Parent!\n");
     p = fork();
@@ -24,8 +26,10 @@ int main() {
     } else if (p == 0) {
       pid_t pid2 = getpid();
       srand(pid2);
-      int time2 = (rand()%4)+1;
+      unsigned int time2 = (rand()%4)+1;
       printf("%d: %d sec\n", pid2, time2);
+      sleep(time2);
+      printf("%d finished after %d sec\n", pid2, time2);
     }
   }
 
