4530b4fa8b7c4f94c3193c3fe01128cb35fa243e
David Schwartzberg <dschwarzberg50@stuy.edu>
2024-11-26 14:42:18 -0500

    kind of printing the main finish message

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/wait.h>
 
 int main() {
-  printf("%d about to create 2 child processes\n", getpid());
+  int ppid = getpid();
+  printf("%d about to create 2 child processes\n", ppid);
   pid_t p;
   p = fork();
   if (p == -1) {
@@ -13,12 +15,11 @@ int main() {
   } else if (p == 0) {
     pid_t pid1 = getpid();
     srand(pid1);
-    unsigned int time1 = (rand()%4)+1;
+    unsigned int time1 = (rand()%5)+1;
     printf("%d: %d sec\n", pid1, time1);
     sleep(time1);
     printf("%d finished after %d sec\n", pid1, time1);
   } else {
-    printf("Hello from Parent!\n");
     p = fork();
     if (p == -1) {
       perror("Failed to fork");
@@ -26,10 +27,19 @@ int main() {
     } else if (p == 0) {
       pid_t pid2 = getpid();
       srand(pid2);
-      unsigned int time2 = (rand()%4)+1;
+      unsigned int time2 = (rand()%5)+1;
       printf("%d: %d sec\n", pid2, time2);
       sleep(time2);
       printf("%d finished after %d sec\n", pid2, time2);
+    } else {
+      int status;
+      wait(&status);
+      printf("main program here\n");
+      printf("WEXITSTATUS: %d\n", WEXITSTATUS(status));
+      printf("WIFEXITED: %d\n", WIFEXITED(status));
+      printf("WIFSIGNALED: %d\n", WIFSIGNALED(status));
+      printf("WTERMSIG: %d\n", WTERMSIG(status));
+      printf("Main Process %d is done. Child _EXITING_CHILD_PID_ slept for _SEC_ sec", getpid());
     }
   }
 
